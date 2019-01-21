fc1506e9077e99f684ec28ac83c47b8213cb2848
howmuchadollarcost <muluyonas@yahoo.com>
2019-01-21 17:07:59 +0100

    measure time

--- FinalGrade/FinalGrade.java
+++ FinalGrade/FinalGrade.java
@@ -1,31 +1,41 @@
 class FinalGrade {
     public static void main(String[] args) {
 
-        System.out.println(finalGrade(100, 12)); 
-        System.out.println(finalGrade(99, 0)); 
-        System.out.println(finalGrade(10, 15)); 
+        long startTime = System.nanoTime();
 
-        System.out.println(finalGrade(85, 5)); 
-
-        System.out.println(finalGrade(55, 3)); 
-
+        System.out.println(finalGrade(100, 12));
+        System.out.println(finalGrade(99, 0));
+        System.out.println(finalGrade(10, 15));
+        System.out.println(finalGrade(85, 5));
+        System.out.println(finalGrade(55, 3));
         System.out.println(finalGrade(55, 0));
-        System.out.println(finalGrade(20, 2)); 
+        System.out.println(finalGrade(20, 2));
+
+        long endTime = System.nanoTime();
+
+        long totalTime = endTime - startTime;
+        System.out.println("Time: " + totalTime);       
 
     }
 
     public static int finalGrade(int grade, int projects) {
         int full;
         if (grade > 90 || projects > 10) {
-            full = 100;
-        } else if (grade > 75 && projects >= 5) {
-            full = 90;
-        } else if (grade > 50 && projects >= 2) {
-            full = 75;
+            return 100;
+        }if (grade > 75 && projects >= 5) {
+            return 90;
+        }if (grade > 50 && projects >= 2) {
+            return 75;
         } else {
-            full = 0;
+            return 0;
         }
-        return full;
     }
 
 }
+
+
+
+
+ 
+
+ 
